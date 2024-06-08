5cc77b6cf254be0091ac2db0a3b7dfbf13806d86
Davide Botti <davidebotti92@gmail.com>
2024-06-08 20:13:21 +0200

    ec2 with docker

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,21 @@
 resource "aws_instance" "example_server" {
-  ami           = "ami-09e647bf7a368e505"
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.public_subnets[0].id
+  ami                         = "ami-09e647bf7a368e505"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_subnets[0].id
   associate_public_ip_address = true
-  security_groups = [ aws_security_group.security.id ]
-  key_name = "example-1"
+  security_groups             = [aws_security_group.security.id]
+  key_name                    = "example-1"
+  iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
+
+  user_data = <<-EOF
+    #!/bin/bash
+    set -ex
+    sudo yum update -y
+    sudo yum install docker -y
+    sudo service docker start
+    sudo usermod -a -G docker ec2-user
+    aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 166222693886.dkr.ecr.eu-central-1.amazonaws.com
+  EOF
 
   tags = {
     Name = "ec2-vpc-example"
---
+++ role.tf
@@ -0,0 +1,49 @@
+resource "aws_iam_role" "ec2_role" {
+  name = "ec2_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+
+  tags = {
+    project = "example"
+  }
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "ec2_profile"
+  role = aws_iam_role.ec2_role.name
+}
+
+resource "aws_iam_role_policy" "ec2_policy" {
+  name = "ec2_policy"
+  role = aws_iam_role.ec2_role.id
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "ecr:GetAuthorizationToken",
+        "ecr:BatchGetImage",
+        "ecr:GetDownloadUrlForLayer"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
