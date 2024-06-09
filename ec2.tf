4f4e571969af3c2b795252185e6f2138fad4ac94
Davide Botti <davidebotti92@gmail.com>
2024-06-09 17:29:34 +0200

    Refactor

--- ec2.tf
+++ ec2.tf
@@ -7,15 +7,7 @@ resource "aws_instance" "example_server" {
   key_name                    = "example-1"
   iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
 
-  user_data = <<-EOF
-    #!/bin/bash
-    set -ex
-    sudo yum update -y
-    sudo yum install docker -y
-    sudo service docker start
-    sudo usermod -a -G docker ec2-user
-    aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 166222693886.dkr.ecr.eu-central-1.amazonaws.com
-  EOF
+  user_data = var.user_data
 
   tags = {
     Name = "ec2-vpc-example"
--- variables.tf
+++ variables.tf
@@ -1,17 +1,31 @@
 variable "public_subnet_cidrs" {
   type        = list(string)
   description = "Public Subnet CIDR values"
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  default     = ["10.0.1.0/24"]
 }
 
 variable "private_subnet_cidrs" {
   type        = list(string)
   description = "Private Subnet CIDR values"
-  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  default     = ["10.0.4.0/24"]
 }
 
 variable "azs" {
   type        = list(string)
   description = "Availability Zones"
-  default     = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+  default     = ["eu-central-1a"]
+}
+
+variable "user_data" {
+  type = string
+  description = "Initialization script"
+  default = <<-EOF
+    #!/bin/bash
+    set -ex
+    sudo yum update -y
+    sudo yum install docker -y
+    sudo service docker start
+    sudo usermod -a -G docker ec2-user
+    aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 166222693886.dkr.ecr.eu-central-1.amazonaws.com
+  EOF
 }
