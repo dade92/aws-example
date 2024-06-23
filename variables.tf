4b2668e99dd2ed2b8bcd1f9250e02aaa5af8297c
Davide Botti <davidebotti92@gmail.com>
2024-06-23 11:16:32 +0200

    Proper script to inject secrets

--- variables.tf
+++ variables.tf
@@ -42,7 +42,9 @@ variable "user_data_BE" {
     sudo service docker start
     sudo usermod -a -G docker ec2-user
     aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 166222693886.dkr.ecr.eu-central-1.amazonaws.com
-    docker run -p 8080:8080 --rm -d public.ecr.aws/j9p4t0r0/spring-example2:latest
+    TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`  && curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/iam/security-credentials/ec2_role > permissions.json
+    docker run -p 8080:8080 --rm -d -e ACCESS_KEY=$(jq -r '.AccessKeyId' permissions.json) -e SECRET_ACCESS_KEY=$(jq -r '.SecretAccessKey' permissions.json) -e SESSION_TOKEN=$(jq -r '.Token' permissions.json) public.ecr.aws/j9p4t0r0/spring-example2:latest
+    rm permissions.json
   EOF
 }
 
