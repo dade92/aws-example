6db330ee24b14c4a5e714c3c96077369eea6a101
dbotti <davide.botti@lastminute.com>
2024-08-01 09:03:33 +0200

    Modifies s3 permission to be publicly available

--- s3.tf
+++ s3.tf
@@ -1,4 +1,30 @@
 resource "aws_s3_bucket" "s3-bucket" {
-  bucket = "s3-bucket-example-1234599"
+  bucket        = "s3-bucket-example-1234599"
   force_destroy = true
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.s3-bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "public_bucket_policy" {
+  bucket = aws_s3_bucket.s3-bucket.id
+
+  depends_on = [aws_s3_bucket_public_access_block.public_access]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.s3-bucket.arn}/*"
+      },
+    ]
+  })
 }
