38ea383893a9dc593f450bd452252734aeb397dc
dbotti <davide.botti@lastminute.com>
2024-08-05 19:43:05 +0200

    Record for the hosted zone of my domain davidebotti.com

--- route.tf
+++ route.tf
@@ -3,10 +3,13 @@
 #   comment = "Example hosted Zone"
 # }
 
-# resource "aws_route53_record" "new-dev" {
-#   zone_id = aws_route53_zone.zone_dev.zone_id
-#   name    = "backend-example.com"
-#   type    = "CNAME"
-#   ttl     = 300
-#   records = [aws_lb.my_alb.dns_name]
-# }
+resource "aws_route53_record" "domain-record" {
+  zone_id = "Z01401972E5NVI2LD2MZT"
+  name    = "davidebotti.com"
+  type    = "A"
+  alias {
+    name                   = aws_lb.my_alb.dns_name
+    zone_id                = aws_lb.my_alb.zone_id
+    evaluate_target_health = true
+  }
+}
