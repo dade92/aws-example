ee6a1438708223c056d5f0f336d66f328dfef44a
dbotti <davide.botti@lastminute.com>
2024-08-19 18:00:37 +0200

    Use the bucket names to create the DNS record

--- route.tf
+++ route.tf
@@ -11,8 +11,8 @@ resource "aws_route53_record" "domain-record" {
 
 resource "aws_route53_record" "images-record" {
   zone_id = "Z01401972E5NVI2LD2MZT"
-  name    = "images.davidebotti.com"
+  name    = aws_s3_bucket.s3-bucket.bucket
   type    = "CNAME"
-  records = ["images.davidebotti.com.s3.eu-central-1.amazonaws.com"]
+  records = [aws_s3_bucket.s3-bucket.bucket_regional_domain_name]
   ttl     = 300
 }
